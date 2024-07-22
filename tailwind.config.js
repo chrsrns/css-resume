ecad0e80d1e12d064b6a1e645556c8d4bd151fb3
Christian Louise B. Aranas <aranaschristianlouise@gmail.com>
2024-07-22 16:40:24 +0800

    Added github link to the source

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,22 @@ module.exports = {
         print: { raw: "print" },
         screen: { raw: "screen" },
       },
+      animation: {
+        "octocat-wave": "octocat-wave 560ms ease-in-out",
+      },
+      keyframes: {
+        "octocat-wave": {
+          "0%, 100%": {
+            transform: "rotate(0)",
+          },
+          "20%, 60%": {
+            transform: "rotate(-25deg)",
+          },
+          "40%, 80%": {
+            transform: "rotate(10deg)",
+          },
+        },
+      },
     },
   },
   plugins: [],
